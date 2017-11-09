0e854b9243dd4de699109fda090f41644943e03f
MERCOIN <mercoinweb@gmail.com>
2017-11-09 08:55:58 +0100

    version 1.4 Mercoin

--- src/main.h
+++ src/main.h
@@ -29,7 +29,7 @@ class CNode;
 
 class CTxMemPool;
 
-static const int LAST_POW_BLOCK = 100000;
+static const int LAST_POW_BLOCK = 100000000;
 
 /** The maximum allowed size for a serialized block, in bytes (network rule) */
 static const unsigned int MAX_BLOCK_SIZE = 1000000;
--- src/version.cpp
+++ src/version.cpp
@@ -9,10 +9,10 @@
 // Name of client reported in the 'version' message. Report the same name
 // for both bitcoind and bitcoin-qt, to make it harder for attackers to
 // target servers or GUI users specifically.
-const std::string CLIENT_NAME("Mercoin-Security");
+const std::string CLIENT_NAME("Mercoin-Security-Reloaded");
 
 // Client version number
-#define CLIENT_VERSION_SUFFIX   "1.5"
+#define CLIENT_VERSION_SUFFIX   "1.5.2"
 
 
 // The following part of the code determines the CLIENT_BUILD variable.
